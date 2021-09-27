1d2fe58dd9b443626bab8b28c76b834a94bc211a
Developer User <mmuehlh@sub.uni-goettingen.de>
2021-09-27 08:53:15 +0200

    Upload-Step: remove input-title and primary checkbox if not html existent

---
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/utils.js
@@ -0,0 +1,37 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+(function($) {
+    DSpace.getTemplate = function(name) {
+        if (DSpace.dev_mode || DSpace.templates === undefined || DSpace.templates[name] === undefined) {
+            $.ajax({
+                url : DSpace.theme_path + 'templates/' + name + '.hbs',
+                success : function(data) {
+                    if (DSpace.templates === undefined) {
+                        DSpace.templates = {};
+                    }
+                    DSpace.templates[name] = Handlebars.compile(data);
+                },
+                async : false
+            });
+        }
+        return DSpace.templates[name];
+    };
+		$(document).ready(function(){
+				if ($('#aspect_submission_StepTransformer_field_file').length) {
+						$('#aspect_submission_StepTransformer_field_file').removeAttr('title');
+					
+						if (!$('#aspect_submission_StepTransformer_table_submit-upload-summary a:contains(".html")').length) {
+
+                $("#aspect_submission_StepTransformer_table_submit-upload-summary th:first-child").hide();
+                $("#aspect_submission_StepTransformer_table_submit-upload-summary td:first-child").hide();
+            }
+
+				}
+			 
+		});
+})(jQuery);
